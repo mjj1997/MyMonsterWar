9846748013220f1c36748bd7cbba94ac3ec0df13
Template Janitor <template.janitor@example.com>
2026-06-25 16:55:00 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -6,7 +6,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(MyMonsterWar_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -57,110 +57,110 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(MyMonsterWar_setup_options)
+  option(MyMonsterWar_ENABLE_HARDENING "Enable hardening" ON)
+  option(MyMonsterWar_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    MyMonsterWar_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    MyMonsterWar_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  MyMonsterWar_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR MyMonsterWar_PACKAGING_MAINTAINER_MODE)
+    option(MyMonsterWar_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(MyMonsterWar_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(MyMonsterWar_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(MyMonsterWar_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(MyMonsterWar_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(MyMonsterWar_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(MyMonsterWar_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(MyMonsterWar_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(MyMonsterWar_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(MyMonsterWar_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(MyMonsterWar_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(MyMonsterWar_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(MyMonsterWar_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(MyMonsterWar_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(MyMonsterWar_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(MyMonsterWar_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(MyMonsterWar_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(MyMonsterWar_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(MyMonsterWar_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(MyMonsterWar_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(MyMonsterWar_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(MyMonsterWar_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(MyMonsterWar_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(MyMonsterWar_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      MyMonsterWar_ENABLE_IPO
+      MyMonsterWar_WARNINGS_AS_ERRORS
+      MyMonsterWar_ENABLE_SANITIZER_ADDRESS
+      MyMonsterWar_ENABLE_SANITIZER_LEAK
+      MyMonsterWar_ENABLE_SANITIZER_UNDEFINED
+      MyMonsterWar_ENABLE_SANITIZER_THREAD
+      MyMonsterWar_ENABLE_SANITIZER_MEMORY
+      MyMonsterWar_ENABLE_UNITY_BUILD
+      MyMonsterWar_ENABLE_CLANG_TIDY
+      MyMonsterWar_ENABLE_CPPCHECK
+      MyMonsterWar_ENABLE_COVERAGE
+      MyMonsterWar_ENABLE_PCH
+      MyMonsterWar_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  MyMonsterWar_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (MyMonsterWar_ENABLE_SANITIZER_ADDRESS OR MyMonsterWar_ENABLE_SANITIZER_THREAD OR MyMonsterWar_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(MyMonsterWar_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(MyMonsterWar_global_options)
+  if(MyMonsterWar_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    MyMonsterWar_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  MyMonsterWar_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(MyMonsterWar_ENABLE_HARDENING AND MyMonsterWar_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR MyMonsterWar_ENABLE_SANITIZER_UNDEFINED
+       OR MyMonsterWar_ENABLE_SANITIZER_ADDRESS
+       OR MyMonsterWar_ENABLE_SANITIZER_THREAD
+       OR MyMonsterWar_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${MyMonsterWar_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${MyMonsterWar_ENABLE_SANITIZER_UNDEFINED}")
+    MyMonsterWar_enable_hardening(MyMonsterWar_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(MyMonsterWar_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(MyMonsterWar_warnings INTERFACE)
+  add_library(MyMonsterWar_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  MyMonsterWar_set_project_warnings(
+    MyMonsterWar_warnings
+    ${MyMonsterWar_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -171,66 +171,66 @@ macro(myproject_local_options)
 
   if(NOT EMSCRIPTEN)
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    MyMonsterWar_enable_sanitizers(
+      MyMonsterWar_options
+      ${MyMonsterWar_ENABLE_SANITIZER_ADDRESS}
+      ${MyMonsterWar_ENABLE_SANITIZER_LEAK}
+      ${MyMonsterWar_ENABLE_SANITIZER_UNDEFINED}
+      ${MyMonsterWar_ENABLE_SANITIZER_THREAD}
+      ${MyMonsterWar_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(MyMonsterWar_options PROPERTIES UNITY_BUILD ${MyMonsterWar_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(MyMonsterWar_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      MyMonsterWar_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(MyMonsterWar_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    MyMonsterWar_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(MyMonsterWar_ENABLE_CLANG_TIDY)
+    MyMonsterWar_enable_clang_tidy(MyMonsterWar_options ${MyMonsterWar_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(MyMonsterWar_ENABLE_CPPCHECK)
+    MyMonsterWar_enable_cppcheck(${MyMonsterWar_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(MyMonsterWar_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    MyMonsterWar_enable_coverage(MyMonsterWar_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(MyMonsterWar_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(MyMonsterWar_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(MyMonsterWar_ENABLE_HARDENING AND NOT MyMonsterWar_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR MyMonsterWar_ENABLE_SANITIZER_UNDEFINED
+       OR MyMonsterWar_ENABLE_SANITIZER_ADDRESS
+       OR MyMonsterWar_ENABLE_SANITIZER_THREAD
+       OR MyMonsterWar_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    MyMonsterWar_enable_hardening(MyMonsterWar_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- cmake/Hardening.cmake
+++ cmake/Hardening.cmake
@@ -1,7 +1,7 @@
 include(CheckCXXCompilerFlag)
 
 macro(
-  myproject_enable_hardening
+  MyMonsterWar_enable_hardening
   target
   global
   ubsan_minimal_runtime)
