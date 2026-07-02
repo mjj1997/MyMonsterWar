c218ab1438be4cf6aa9060487c4465da2c19a925
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-02 16:23:03 +0800

    feat: Add 表示压入、弹出和替换场景事件的结构体

--- src/engine/utils/events.h
+++ src/engine/utils/events.h
@@ -1,8 +1,29 @@
 #pragma once
 
+#include <memory>
+
+namespace engine::scene {
+
+class SceneBase;
+
+} // namespace engine::scene
+
 namespace engine::utils {
 
 struct QuitEvent // 退出事件
 {};
+
+struct PopSceneEvent // 弹出场景事件
+{};
+
+struct PushSceneEvent // 压入场景事件
+{
+    std::unique_ptr<engine::scene::SceneBase> scene;
+};
+
+struct ReplaceSceneEvent // 替换场景事件
+{
+    std::unique_ptr<engine::scene::SceneBase> scene;
+};
 
 } // namespace engine::utils
