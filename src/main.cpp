52130b005a491ccf4a3f825504d01be4084bc5e1
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-02 16:55:50 +0800

    feat: Add 发送压入场景事件信号的逻辑

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include "engine/core/context.h"
 #include "engine/core/game_app.h"
+#include "engine/utils/events.h"
 #include "game/scene/game_scene.h"
 
+#include <entt/signal/dispatcher.hpp>
 #include <spdlog/spdlog.h>
 
 void setupInitialScene(engine::core::Context& context)
 {
     // GameApp在调用run方法之前，先创建并设置初始场景
     auto initialScene = std::make_unique<game::scene::GameScene>(context);
+    // 发送压入场景事件信号，将初始场景压入栈顶
+    context.dispatcher().trigger<engine::utils::PushSceneEvent>(
+        engine::utils::PushSceneEvent{ std::move(initialScene) });
 }
 
 int main(int /*argc*/, char* /*argv*/[])
