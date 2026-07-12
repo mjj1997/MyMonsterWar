0d9b835f31b426676b30721cd768759dbea60825
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-12 18:36:39 +0800

    feat(RenderSystem): Update 排序渲染组件，并严格按照渲染组件的顺序执行实体渲染

--- src/engine/system/render_system.cpp
+++ src/engine/system/render_system.cpp
@@ -1,10 +1,12 @@
 #include "render_system.h"
+#include "../component/render_component.h"
 #include "../component/sprite_component.h"
 #include "../component/transform_component.h"
 #include "../render/camera.h"
 #include "../render/renderer.h"
 
 #include <entt/entity/registry.hpp>
+#include <spdlog/spdlog.h>
 
 namespace engine::system {
 
@@ -12,7 +14,17 @@ void RenderSystem::update(entt::registry& registry,
                           render::Renderer& renderer,
                           const render::Camera& camera)
 {
-    auto view = registry.view<component::TransformComponent, component::SpriteComponent>();
+    spdlog::trace("RenderSystem::update");
+
+    // 对渲染组件进行排序 (需要自定义渲染组件的比较运算符)
+    registry.sort<component::RenderComponent>(
+        [](const auto& lhs, const auto& rhs) { return lhs < rhs; });
+
+    // 执行渲染，注意排序的渲染组件必须放在最前面
+    auto view = registry.view<component::RenderComponent,
+                              component::TransformComponent,
+                              component::SpriteComponent>();
+    view.use<engine::component::RenderComponent>(); // 强制使用渲染组件排序
     for (auto entity : view) {
         const auto& transformComponent = view.get<component::TransformComponent>(entity);
         const auto& spriteComponent = view.get<component::SpriteComponent>(entity);
