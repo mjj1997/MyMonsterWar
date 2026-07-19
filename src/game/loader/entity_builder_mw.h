9afdba556b07231bfc1fa3aa94849c472ec3607a
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-19 09:58:08 +0800

    feat(EntityBuilderMW): Derive from base class BasicEntityBuilder
    本项目 MonsterWar 专用的实体生成器

---
+++ src/game/loader/entity_builder_mw.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "../../engine/loader/basic_entity_builder.h"
+
+namespace game::loader {
+
+/**
+ * @brief 拓展的关卡载入实体生成器
+ * 功能包括：
+ * 1. 生成路径节点和起点。
+ * 2. ...
+ */
+class EntityBuilderMW final : public engine::loader::BasicEntityBuilder
+{
+};
+
+} // namespace game::loader
