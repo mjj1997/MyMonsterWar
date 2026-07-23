a64bfc685dbb38752b65d2a64abbe4f992e36041
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-23 14:33:26 +0800

    feat(Blueprint): Add 单一动画的蓝图

--- src/game/data/entity_blueprint.h
+++ src/game/data/entity_blueprint.h
@@ -6,6 +6,7 @@
 #include <glm/vec2.hpp>
 
 #include <string>
+#include <vector>
 
 /* 蓝图结构体，为实体工厂提供数据 */
 namespace game::data {
@@ -29,6 +30,14 @@ struct SpriteBlueprint
     glm::vec2 m_size{ 0.0F };
     glm::vec2 m_offset{ 0.0F };
     bool m_isFacedRight{ true }; ///< @brief 角色图片默认朝右，如果朝左就设置为false
+};
+
+/// @brief 单一动画的蓝图，多个蓝图构成的关联容器即可用于创建动画组件
+struct AnimationBlueprint
+{
+    float m_durationPerFrame{ 0.0F };
+    int m_row{ 0 };
+    std::vector<int> m_frameIndices; ///< @brief 动画帧索引数组
 };
 
 } // namespace game::data
