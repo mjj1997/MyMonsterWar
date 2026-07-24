01544a3b8b251d19017ca3d27f42319ec0eee34e
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-24 16:19:13 +0800

    feat(Math): Add 根据等级和稀有度修改属性的自由函数

--- src/engine/utils/math.h
+++ src/engine/utils/math.h
@@ -96,4 +96,17 @@ inline int randomInt(int min, int max)
     return distribution(generator);
 }
 
+/**
+ * @brief 根据等级和稀有度修改属性
+ * @param base 基础属性
+ * @param level 等级
+ * @param rarity 稀有度
+ * @return 修改后的属性
+ */
+inline float statModify(float base, int level = 1, int rarity = 1)
+{
+    return base * (0.95f + 0.05f * level) * (0.9f + 0.1f * rarity);
+    // NOTE: 未来可改成数据驱动方便调整
+}
+
 } // namespace engine::utils
