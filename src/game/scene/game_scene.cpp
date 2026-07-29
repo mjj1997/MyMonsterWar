43ea1b8b30ef4b5443f4f68864b494e8123955a1
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-29 22:51:28 +0800

    feat(GameScene): Update 各系统更新逻辑

--- src/game/scene/game_scene.cpp
+++ src/game/scene/game_scene.cpp
@@ -87,9 +87,16 @@ void GameScene::update(float deltaTime)
     // 每一帧最先清理死亡实体(要在 dispatcher 处理完事件后再清理, 因此放在下一帧开头)
     m_removeDeadSystem->update(m_registry);
 
-    // 注意系统更新顺序, 路径跟随系统要放到移动系统之前
-    m_followPathSystem->update(m_registry, dispatcher, m_pathNodes);
+    /* 注意系统更新顺序 */
+    m_timerSystem->update(m_registry, deltaTime);
+    // 注意阻挡要发生在移动之前，否则敌人本帧会先移动一段距离再被停下，表现会“滑一下”
     m_blockSystem->update(m_registry, dispatcher);
+    m_setTargetSystem->update(m_registry);
+    // 路径跟随系统要放到移动系统之前
+    m_followPathSystem->update(m_registry, dispatcher, m_pathNodes);
+    // 朝向系统要放到阻挡、设置目标、路径跟随系统之后
+    m_orientationSystem->update(m_registry);
+    m_attackStarterSystem->update(m_registry, dispatcher);
     m_movementSystem->update(m_registry, deltaTime);
     m_ySortSystem->update(m_registry); // 调用顺序要放到移动系统之后
     m_animationSystem->update(deltaTime);
