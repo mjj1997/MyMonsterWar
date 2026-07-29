aa70b9e046316c5ae6615ac10d31fd8be2696e91
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-29 15:41:53 +0800

    feat(AttackStarterSystem): Add 处理被阻挡敌人角色的私有方法

--- src/game/system/attack_starter_system.cpp
+++ src/game/system/attack_starter_system.cpp
@@ -1,7 +1,33 @@
 #include "attack_starter_system.h"
+#include "../component/blocked_by_component.h"
+#include "../component/enemy_component.h"
+#include "../defs/tags.h"
+
+#include "../../engine/utils/events.h"
+
+#include <entt/entity/registry.hpp>
+#include <entt/signal/dispatcher.hpp>
+
+using namespace entt::literals;
 
 namespace game::system {
 
 void AttackStarterSystem::update(entt::registry& registry, entt::dispatcher& dispatcher) {}
+
+void AttackStarterSystem::updateEnemyBlocked(entt::registry& registry, entt::dispatcher& dispatcher)
+{
+    // 筛选条件：被阻挡的敌人角色，且攻击就绪
+    auto blockedEnenmyView = registry.view<game::component::EnemyComponent,
+                                           game::component::BlockedByComponent,
+                                           game::defs::AttackReadyTag>();
+
+    for (auto enemyEntity : blockedEnenmyView) {
+        // 添加动作锁定标签（确保攻击动画执行完毕再进行其它动作）
+        registry.emplace_or_replace<game::defs::ActionLockedTag>(enemyEntity);
+        // 每次攻击后，移除攻击就绪标签，攻击冷却重新计时
+        registry.remove<game::defs::AttackReadyTag>(enemyEntity);
+        dispatcher.enqueue(engine::utils::PlayAnimationEvent{ enemyEntity, "attack"_hs, false });
+    }
+}
 
 } // namespace game::system
--- src/game/system/attack_starter_system.h
+++ src/game/system/attack_starter_system.h
@@ -12,6 +12,11 @@ class AttackStarterSystem
 {
 public:
     void update(entt::registry& registry, entt::dispatcher& dispatcher);
+
+private:
+    /* 拆分逻辑的函数，在 update 中调用 */
+    /// @brief 处理被阻挡的敌人角色
+    void updateEnemyBlocked(entt::registry& registry, entt::dispatcher& dispatcher);
 };
 
 } // namespace game::system
