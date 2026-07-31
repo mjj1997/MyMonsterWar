630217fd9caeec9412570bd681472ea41ad82f3d
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-31 09:38:18 +0800

    feat(AnimationKeyframeEventSystem): Update 发送攻击或治疗事件的逻辑

--- src/game/system/animation_keyframe_event_system.cpp
+++ src/game/system/animation_keyframe_event_system.cpp
@@ -2,7 +2,10 @@
 #include "../component/blocked_by_component.h"
 #include "../component/enemy_component.h"
 #include "../component/player_component.h"
+#include "../component/stats_component.h"
 #include "../component/target_component.h"
+#include "../defs/events.h"
+#include "../defs/tags.h"
 
 #include <entt/entity/registry.hpp>
 #include <entt/signal/dispatcher.hpp>
@@ -41,6 +44,17 @@ void AnimationKeyframeEventSystem::handleHitEvent(const engine::utils::Animation
         if (auto* targetComponent = m_registry.try_get<game::component::TargetComponent>(
                 event.m_entity);
             targetComponent != nullptr) {
+            const auto& statsComponent = m_registry.get<game::component::StatsComponent>(
+                event.m_entity);
+            // 根据玩家的类型，发送攻击或治疗事件
+            if (m_registry.all_of<game::defs::HealerTag>(event.m_entity)) {
+                m_dispatcher.enqueue(game::defs::HealEvent{
+                    event.m_entity, targetComponent->m_entity, statsComponent.m_atk });
+            } else {
+                m_dispatcher.enqueue(game::defs::AttackEvent{
+                    event.m_entity, targetComponent->m_entity, statsComponent.m_atk });
+            }
+            // TODO: 播放“hit”音效
         }
         return;
     }
@@ -51,7 +65,14 @@ void AnimationKeyframeEventSystem::handleHitEvent(const engine::utils::Animation
         if (auto* blockedByComponent = m_registry.try_get<game::component::BlockedByComponent>(
                 event.m_entity);
             blockedByComponent != nullptr) {
+            const auto& statsComponent = m_registry.get<game::component::StatsComponent>(
+                event.m_entity);
+            // 发送攻击事件
+            m_dispatcher.enqueue(game::defs::AttackEvent{
+                event.m_entity, blockedByComponent->m_entity, statsComponent.m_atk });
         }
+        // NOTE: 只有远程敌人才有目标组件，但远程攻击动画事件 ID 为 “emit”，不在这里处理
+        // NOTE: 敌人击中事件不播放音效，未来可按需添加
         return;
     }
 }
