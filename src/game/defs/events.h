8c6d7f593d46cf69a2e5d983d596e41a0cb9a1d4
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-07-31 09:29:36 +0800

    feat(Events): Add 攻击命中事件和治疗命中事件

--- src/game/defs/events.h
+++ src/game/defs/events.h
@@ -1,8 +1,26 @@
 #pragma once
 
+#include <entt/entity/entity.hpp>
+
 namespace game::defs {
 
 struct EnemyArriveBaseEvent // 敌人到达基地事件
 {};
+
+/// @brief 攻击（命中）事件
+struct AttackEvent
+{
+    entt::entity m_attacker{ entt::null }; ///< @brief 攻击者
+    entt::entity m_target{ entt::null };   ///< @brief 目标
+    float m_damage{};                      ///< @brief 原始伤害
+};
+
+/// @brief 治疗（命中）事件
+struct HealEvent
+{
+    entt::entity m_healer{ entt::null }; ///< @brief 治疗者
+    entt::entity m_target{ entt::null }; ///< @brief 目标
+    float m_amount{};                    ///< @brief 治疗量
+};
 
 } // namespace game::defs
