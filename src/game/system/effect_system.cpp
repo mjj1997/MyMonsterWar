9d115932ed2d033bc34b86cb5b8f724df06fe6f7
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-08-07 16:49:16 +0800

    feat(EffectSystem): Update 敌人死亡特效事件与处理函数的绑定和解绑

--- src/game/system/effect_system.cpp
+++ src/game/system/effect_system.cpp
@@ -2,6 +2,8 @@
 #include "../defs/events.h"
 #include "../factory/entity_factory.h"
 
+#include <entt/signal/dispatcher.hpp>
+
 namespace game::system {
 
 EffectSystem::EffectSystem(entt::registry& registry,
@@ -11,10 +13,13 @@ EffectSystem::EffectSystem(entt::registry& registry,
     , m_dispatcher{ dispatcher }
     , m_entityFactory{ entityFactory }
 {
+    m_dispatcher.sink<game::defs::EnemyDeadEffectEvent>()
+        .connect<&EffectSystem::handleEnemyDeadEffectEvent>(this);
 }
 
 EffectSystem::~EffectSystem()
 {
+    m_dispatcher.disconnect(this);
 }
 
 void EffectSystem::handleEnemyDeadEffectEvent(const game::defs::EnemyDeadEffectEvent& event)
