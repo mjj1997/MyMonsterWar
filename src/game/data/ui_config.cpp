64fbb618887f3c4462f9a538338e2787565276d7
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-08-10 17:16:30 +0800

    feat(UiConfig): Add 图标、肖像、肖像框的映射以及玩家单位面板的数据到数据成员中并提供相应的 getters

--- src/game/data/ui_config.cpp
+++ src/game/data/ui_config.cpp
@@ -1,5 +1,37 @@
 #include "ui_config.h"
 
+#include <spdlog/spdlog.h>
+
 namespace game::data {
+
+engine::render::Image& UiConfig::icon(entt::id_type id)
+{
+    if (auto it = m_icons.find(id); it != m_icons.end()) {
+        return it->second;
+    } else {
+        spdlog::error("未找到职业类型 ID: {} 的图标", id);
+        return m_icons.begin()->second;
+    }
+}
+
+engine::render::Image& UiConfig::portrait(entt::id_type id)
+{
+    if (auto it = m_portraits.find(id); it != m_portraits.end()) {
+        return it->second;
+    } else {
+        spdlog::error("未找到角色名 ID: {} 的肖像", id);
+        return m_portraits.begin()->second;
+    }
+}
+
+engine::render::Image& UiConfig::portraitFrame(int rarity)
+{
+    if (auto it = m_portraitFrames.find(rarity); it != m_portraitFrames.end()) {
+        return it->second;
+    } else {
+        spdlog::error("未找到稀有度: {} 的肖像框", rarity);
+        return m_portraitFrames.begin()->second;
+    }
+}
 
 } // namespace game::data
--- src/game/data/ui_config.h
+++ src/game/data/ui_config.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "../../engine/render/image.h"
+
+#include <entt/entity/fwd.hpp>
+#include <glm/vec2.hpp>
+
+#include <unordered_map>
+
 namespace game::data {
 
 /**
@@ -9,6 +16,37 @@ namespace game::data {
  */
 class UiConfig
 {
+public:
+    UiConfig() = default;
+    ~UiConfig() = default;
+
+    // --- Getters ---
+    [[nodiscard]] engine::render::Image& icon(entt::id_type id);
+    [[nodiscard]] engine::render::Image& portrait(entt::id_type id);
+    [[nodiscard]] engine::render::Image& portraitFrame(int rarity);
+    [[nodiscard]] float playerUnitPanelPadding() const { return m_playerUnitPanelPadding; }
+    [[nodiscard]] glm::vec2 playerUnitPanelFrameSize() const { return m_playerUnitPanelFrameSize; }
+    [[nodiscard]] int playerUnitPanelFontSize() const { return m_playerUnitPanelFontSize; }
+    [[nodiscard]] std::string playerUnitPanelFontPath() const { return m_playerUnitPanelFontPath; }
+    [[nodiscard]] glm::vec2 playerUnitPanelFontOffset() const
+    {
+        return m_playerUnitPanelFontOffset;
+    }
+
+private:
+    /// @brief 储存职业类型的图标（职业类型ID:图标）
+    std::unordered_map<entt::id_type, engine::render::Image> m_icons;
+    /// @brief 储存角色肖像（角色名ID:肖像）
+    std::unordered_map<entt::id_type, engine::render::Image> m_portraits;
+    /// @brief 储存角色肖像框（稀有度:肖像框）
+    std::unordered_map<int, engine::render::Image> m_portraitFrames;
+
+    // --- 玩家单位面板的配置数据（从 JSON 配置文件中的 layout 项读取） ---
+    float m_playerUnitPanelPadding{ 10.0F };                ///< @brief 玩家单位面板间隔
+    glm::vec2 m_playerUnitPanelFrameSize{ 128.0F, 128.0F }; ///< @brief 玩家单位面板大小
+    int m_playerUnitPanelFontSize{ 40 };                    ///< @brief 玩家单位面板字体大小
+    std::string m_playerUnitPanelFontPath;                  ///< @brief 玩家单位面板字体路径
+    glm::vec2 m_playerUnitPanelFontOffset{ 16.0F, 72.0F };  ///< @brief 玩家单位面板字体偏移
 };
 
 } // namespace game::data
