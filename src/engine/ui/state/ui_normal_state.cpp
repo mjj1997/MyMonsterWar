20583f981d64b744408f57d4e1bedefd569aea93
mjj1997 <45837504+mjj1997@users.noreply.github.com>
2026-08-13 16:11:46 +0800

    chore(UiNormalState): Update 函数调用名和音效名称

--- src/engine/ui/state/ui_normal_state.cpp
+++ src/engine/ui/state/ui_normal_state.cpp
@@ -5,6 +5,7 @@
 #include "ui_hover_state.h"
 
 #include <entt/core/hashed_string.hpp>
+#include <spdlog/spdlog.h>
 
 using namespace entt::literals;
 
@@ -13,7 +14,9 @@ namespace engine::ui::state {
 void UiNormalState::enter()
 {
     // 设置 UI 为正常状态的图片
-    m_owner->setImage("normal"_hs);
+    m_owner->setCurrentImage("normal"_hs);
+
+    spdlog::debug("切换到正常状态");
 }
 
 void UiNormalState::update(float deltaTime, engine::core::Context& context)
@@ -22,7 +25,7 @@ void UiNormalState::update(float deltaTime, engine::core::Context& context)
     const auto& mousePos = inputManager.logicalMousePosition();
     if (m_owner->isPointInside(mousePos)) { // 如果鼠标在 UI 元素内，切换到悬停状态
         // 播放悬停时的音效
-        m_owner->playSound("hover"_hs);
+        m_owner->playSound("ui_hover"_hs);
         m_owner->setNextState(std::make_unique<UiHoverState>(m_owner));
     }
 }
